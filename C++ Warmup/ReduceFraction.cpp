37340b46ed9bd258410ef839f58b2d22b7de1330
AlecSitu <epicsitu@gmail.com>
2025-04-02 22:09:27 -0700

    began reduceFraction function

---
+++ C++ Warmup/ReduceFraction.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Epics on 4/2/2025.
+//
+
+#include "ReduceFraction.h"
+#include <iostream>
+
+std::string reduceFraction(std::string fraction) {
+
+    return "";
+}
+
+int main() {
+
+    return 0;
+}
---
+++ C++ Warmup/ReduceFraction.h
@@ -0,0 +1,17 @@
+//
+// Created by Epics on 4/2/2025.
+//
+
+#ifndef REDUCEFRACTION_H
+#define REDUCEFRACTION_H
+
+
+
+class ReduceFraction {
+
+
+};
+
+
+
+#endif //REDUCEFRACTION_H
